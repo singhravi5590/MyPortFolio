7dadb6d3c1330e2ee341d924a92dc31c67d8b703
singhravi5590 <singhravi5590@gmail.com>
2025-08-13 13:19:08 +0530

    Education section completed

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ const App = () => {
         <Skills />
         <Experience />
         <Work />
-        {/*<Education />
-        <Contact />
+        <Education />
+        {/*<Contact />
         <Footer /> */}
       </div>
 
